6615a9d53f282be68e2b5675cd243d6d6b2e0c41
jangelpedreira <78357188+jangelpedreira@users.noreply.github.com>
2022-06-22 13:05:39 +0200

    Commit

--- widgets/CalculateNaturalAfections/Widget.js
+++ widgets/CalculateNaturalAfections/Widget.js
@@ -36,8 +36,7 @@ define(['dojo/_base/declare',
   function (declare, _WidgetsInTemplateMixin, BaseWidget, NumberTextBox,
     NumberSpinner, ComboBox, TextBox, Textarea, Select, dom, domAttr, dojoJson,
     html, lang, domStyle, on, Geoprocessor, JobInfo, esriRequest) {
-    
-    //URLs de los servicios de geoprocesamiento alojados en ArcGIS Server
+
     var urlServicioGeoprocSincrono = "https://localhost:6443/arcgis/rest/services/Geoprocesamiento/calcularAfeccionesAmbientalesSincrono/GPServer/Calcular%20afecciones%20ambientales%20de%20las%20infraestructuras%20de%20parques%20e%C3%B3licos";
     var urlServicioGeoprocAsincrono = "https://localhost:6443/arcgis/rest/services/Geoprocesamiento/calcularAfeccionesAmbientalesAsincrono/GPServer/Calcular%20afecciones%20ambientales%20de%20las%20infraestructuras%20de%20parques%20e%C3%B3licos";
 
@@ -60,8 +59,6 @@ define(['dojo/_base/declare',
       },
 
       setTipoInfraestructura: function (event) {
-      // Oculta el campo Distancia Buffer del formulario cuando se selecciona
-      // como Tipo de Infraestructura: "LINA"
         console.log("IN -- setTipoInfraestructura");
         var tipoInfraestructura = this.comboTiposInfraest.value;
         console.log("Tipo infraestructura: " + tipoInfraestructura);
@@ -77,10 +74,7 @@ define(['dojo/_base/declare',
         console.log("OUT -- setTipoInfraestructura");
       },
 
-           
       setExpedientes: function (event) {
-      // Oculta el campo Expediente del formulario cuando se selecciona
-      // para el campo Afeccioneas para: "Todos los expedientes"
         console.log("IN -- setExpedientes");
         var calculoExpedientes = this.comboExpedientes.value;
         console.log("Afecciones para: " + calculoExpedientes);
@@ -101,11 +95,7 @@ define(['dojo/_base/declare',
         console.log("OUT -- setCalculoAfecciones")
       },
 
-      
       calcularAfecciones: function () {
-      // Función para calcular las afecciones en función de los valores
-      // introducidos por el usuario invocando al servicio web de 
-      // geoprocesamiento.
         console.log("IN -- calcularAfecciones");
 
         var that = this;
@@ -134,26 +124,22 @@ define(['dojo/_base/declare',
           if (tipoInfraestructura == "PARQUE")
             distanciaBuffer = this.txtDistanciaBuffer.value;
           console.log("Distancia buffer: " + distanciaBuffer);
-          var operacion = "CALCULAR_AFECCIONES";
+          var operacion = "CONSULTAR_AFECCIONES";
           console.log("Operación: " + operacion);
           //var tipoPatrimonio = this.comboTiposPatrimonio.value;
-		      var tipoPatrimonio = "AMBIENTAL"
+		  var tipoPatrimonio = "AMBIENTAL"
           //console.log("Tipo de patrimonio " + tipoPatrimonio);
 
-          // Se indica como tipo de Geoproceso Asícrono
           var tipoGeoproceso = "ASINCRONO";
 
           // Deshabilitar los controles del formulario
           this.disableFormElements();
 
           if (tipoGeoproceso == 'ASINCRONO') {
-            // El tiopo de geoproceso seleccionado es asícrono
 
-            // Se creaa el geoproceso
             var urlServicio = urlServicioGeoprocAsincrono;
             this.gp = new Geoprocessor(urlServicio);
 
-            // Se pasan los parámetros al Geoproceso  
             var params = {
               "tipoInfraestructura": tipoInfraestructura,
               "numExpediente": numExpediente,
@@ -165,35 +151,30 @@ define(['dojo/_base/declare',
             console.log("Ejecutar geoproceso: " + this.gp.url + " - Parámetros: " + params);
             this.estadoGeoproceso.textContent = "Procesando ...."
             
-            // Se ejecuta el Geoproceso de forma asíncrona
+            //gp.execute(params, mostrarResultados);
             this.gp.submitJob(params);
      
-            // Se definen eventos que se ejecutan cuando el evento se produce durante
-            // la ejecución asíncrona del geoproceso. 
-
-            // Fires when a synchronous GP task is completed
+            //Fires when a synchronous GP task is completed
             this.own(on(this.gp, 'execute-complete', lang.hitch(this, this.onExecuteComplete)));
 
-            // Fires when an asynchronous GP task using submitJob is complete.
+            //Fires when an asynchronous GP task using submitJob is complete.
             this.own(on(this.gp, 'job-complete', lang.hitch(this, this.onJobComplete)));
 
             this.own(on(this.gp, 'job-cancel', lang.hitch(this, this.onJobCancel)));
 
-            // Fires when a job status update is available.
+            //Fires when a job status update is available.
             this.own(on(this.gp, 'status-update', lang.hitch(this, this.onStatusUpdate)));
 
-            // Fires when the result of an asynchronous GP task execution is available.
+            //Fires when the result of an asynchronous GP task execution is available.
             this.own(on(this.gp, 'get-result-data-complete', lang.hitch(this, this.onGetResultDataComplete)));
 
             this.own(on(this.gp, 'error', lang.hitch(this, this.onError)));
           }
           else {
-
-            // Se creaa el geoproceso
             var urlServicio = urlServicioGeoprocSincrono;
+
             this.gp = new Geoprocessor(urlServicio);
 
-            // Se pasan los parámetros al Geoproceso 
             var params = {
               "tipoInfraestructura": tipoInfraestructura,
               "numExpediente": numExpediente,
@@ -205,13 +186,10 @@ define(['dojo/_base/declare',
             console.log("Ejecutar geoproceso: " + this.gp.url + " - Parámetros: " + params);
           
             this.estadoGeoproceso.textContent = "Procesando ...."
-            
-            // Se ejecuta el geoproceso de forma síncrona
+
             this.gp.execute(params, mostrarResultados, errorCallback);
 
             function errorCallback(error) {
-            // Función de callback que se invoca cuando se produce un error   
-            // en el geoproceso
               console.log("IN -- errorCallback (Geoproceso síncrono");
               this.estadoGeoproceso.textContent = error.message;
               this.estadoGeoproceso.style.color = "red";
@@ -223,8 +201,6 @@ define(['dojo/_base/declare',
             }
 
             function mostrarResultados(results, messages) {
-            // Función de callaback que muestra el resultado del Geoproceso cuando termina,
-            // actualizando el forumulario  
               var mensaje = "";
               mensaje = "Geoproceso finalizado correctamente";
               console.log(mensaje);
@@ -258,10 +234,9 @@ define(['dojo/_base/declare',
         console.log("OUT -- calcularAfecciones");
       },
 
-      onStatusUpdate: function (jobInfo) {
       // Función de callback, que se invoca cada cierto tiempo para mostrar
       // el estado del geoproceso
-      
+      onStatusUpdate: function (jobInfo) {
         console.log("IN -- oStatusUpdate");
 
         console.log(jobInfo.jobInfo.jobStatus);
@@ -308,9 +283,7 @@ define(['dojo/_base/declare',
         console.log("OUT -- onStatusUpdate");
       },
 
-
       onJobCancel: function () {
-      // Función que se invoca cuando el geoproceso asícrono se cancela
         console.log("IN -- onJobCancel");
         mensaje = "El geoproceso ha sido cancelado";
         console.log("Resultado: " + mensaje);
@@ -320,7 +293,6 @@ define(['dojo/_base/declare',
       },
 
       onError: function(error) {
-      // Función que se invoca cuando el geoproceso asícrono falla
         console.log("IN -- onError");
         console.log("Error: " + error.error.message);
         this.resultadoGeoproceso.textContent = error.error.message;
@@ -329,7 +301,6 @@ define(['dojo/_base/declare',
       },
 
       onJobComplete: function(jobInfo) {
-      // Función que se invoca cuando el geoproceso asíncrono se completa
         console.log("IN -- onJobComplete");
         this.enableFormElements();
         this.gp.getResultData(jobInfo.jobInfo.jobId, "result");
@@ -337,7 +308,6 @@ define(['dojo/_base/declare',
       },
 
       onGetResultDataComplete: function(result) {
-      // Función que se invoca cuando se obtiene el resultado del geoproceso asíncrono
         console.log("IN -- onGetResultDataComplete");
         console.log("Geoproceso finalizado correctamente");
         var mensaje = "Número de afecciones calculadas: " + result.result.value.features.length;
@@ -348,7 +318,6 @@ define(['dojo/_base/declare',
       },
 
       enableFormElements: function () {
-      // Función que habilita los elementos del formulario tras finalizar la ejecución del geoproceso
         console.log("IN -- enableFormElements");
         html.removeClass("comboTiposInfraest", 'jimu-state-disabled');
         html.removeClass("comboExpedientes", 'jimu-state-disabled');
@@ -359,7 +328,6 @@ define(['dojo/_base/declare',
       },
       
       disableFormElements: function () {
-      // Función que deshabilita los elementos del formulario durante la ejecución del geoproceso
         console.log("IN -- disableFormElements");
         html.addClass("comboTiposInfraest", 'jimu-state-disabled');
         html.addClass("comboExpedientes", 'jimu-state-disabled');
